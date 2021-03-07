a1d183fd3c9390ca9087a3c90cecb2b99f828dfc
Jonathan <jonathanluu122@gmail.com>
2021-03-06 20:34:16 -0500

    cleaned code

--- Code/Payload/Payload.ino
+++ Code/Payload/Payload.ino
@@ -34,42 +34,51 @@
 #endif
 
 
-void setup() {
-
-  SERIAL_PORT.begin(115200);
-  while(!SERIAL_PORT){};
-
-#ifdef USE_SPI
-    SPI_PORT.begin();
-#else
-    WIRE_PORT.begin();
-    WIRE_PORT.setClock(400000);
-#endif
+void init_imu(bool flag)
+{
+  if (flag)
+  {
+    SERIAL_PORT.begin(115200);
+    while(!SERIAL_PORT){};
   
-  bool initialized = false;
-  while( !initialized ){
-
-#ifdef USE_SPI
-    myICM.begin( CS_PIN, SPI_PORT ); 
-#else
-    myICM.begin( WIRE_PORT, AD0_VAL );
-#endif
-
-    SERIAL_PORT.print( F("Initialization of the sensor returned: ") );
-    SERIAL_PORT.println( myICM.statusString() );
-    if( myICM.status != ICM_20948_Stat_Ok ){
-      SERIAL_PORT.println( "Trying again..." );
-      delay(500);
-    }else{
-      initialized = true;
-    }
+    #ifdef USE_SPI
+        SPI_PORT.begin();
+    #else
+        WIRE_PORT.begin();
+        WIRE_PORT.setClock(400000);
+    #endif
+      
+      bool initialized = false;
+      while( !initialized ){
+    
+    #ifdef USE_SPI
+        myICM.begin( CS_PIN, SPI_PORT ); 
+    #else
+        myICM.begin( WIRE_PORT, AD0_VAL );
+    #endif
+    
+        SERIAL_PORT.print( F("Initialization of the sensor returned: ") );
+        SERIAL_PORT.println( myICM.statusString() );
+        if( myICM.status != ICM_20948_Stat_Ok ){
+          SERIAL_PORT.println( "Trying again..." );
+          delay(500);
+        }else{
+          initialized = true;
+        }
+      }
   }
+}
 
+
+void init_sd_card(bool flag)
+{
   // wait for Serial Monitor to connect. Needed for native USB port boards only:
+  if (flag)
+  {
   while (!Serial);
   Serial.print("Initializing SD card...");
 
-  if (!SD.begin(BUILTIN_SDCARD)) {
+    if (!SD.begin(BUILTIN_SDCARD)) {
     Serial.println("initialization failed. Things to check:");
     Serial.println("1. is a card inserted?");
     Serial.println("2. is your wiring correct?");
@@ -77,12 +86,21 @@ void setup() {
     while (true);
   }
   
-  Serial.println("initialization done.");
+  Serial.println("initialization done."); 
+  }
+}
+
+
+void setup() {
+  SERIAL_PORT.begin(115200);
+  
+  init_imu(true);
+  init_sd_card(true); 
+
 }
 
 void loop() {
 
-  String dataString = "2";
   
   if( myICM.dataReady() ){
     myICM.getAGMT();                // The values are only updated when you call 'getAGMT'
@@ -96,7 +114,6 @@ void loop() {
     delay(500);
   }
 
-  logger(dataString);
 }
 
 
