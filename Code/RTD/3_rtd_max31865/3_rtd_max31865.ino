3f8a5cb334650b5cb9827d5e3b7277301f5587a6
Jonathan <jonathanluu122@gmail.com>
2021-05-15 00:39:32 -0400

    spi to communicate with 3 slave devices

---
+++ Code/RTD/3_rtd_max31865/3_rtd_max31865.ino
@@ -0,0 +1,95 @@
+/*************************************************** 
+  This is a library for the Adafruit PT100/P1000 RTD Sensor w/MAX31865
+
+  Designed specifically to work with the Adafruit RTD Sensor
+  ----> https://www.adafruit.com/products/3328
+
+  This sensor uses SPI to communicate, 4 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+#include <Adafruit_MAX31865.h>
+
+// The value of the Rref resistor. Use 430.0 for PT100 and 4300.0 for PT1000
+#define RREF      430.0
+// The 'nominal' 0-degrees-C resistance of the sensor
+// 100.0 for PT100, 1000.0 for PT1000
+#define RNOMINAL  100.0
+#define CS_PIN1 10
+#define CS_PIN2 37
+#define CS_PIN3 36
+
+// Use software SPI: CS, DI, DO, CLK
+//Adafruit_MAX31865 thermo = Adafruit_MAX31865(CS_PIN, 36, 35, 34);
+//Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 11, 12, 13);
+//Adafruit_MAX31865 thermo = Adafruit_MAX31865(10, 9, 8, 7);
+// use hardware SPI, just pass in the CS pin
+//Adafruit_MAX31865 thermo = Adafruit_MAX31865(SCL);
+Adafruit_MAX31865 thermo1 = Adafruit_MAX31865(CS_PIN1);
+Adafruit_MAX31865 thermo2 = Adafruit_MAX31865(CS_PIN2);
+Adafruit_MAX31865 thermo3 = Adafruit_MAX31865(CS_PIN3);
+
+
+void setup() {
+  //Serial.begin(115200);
+  Serial.begin(9600);
+  Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
+
+  thermo1.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  thermo2.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+  thermo3.begin(MAX31865_3WIRE);  // set to 2WIRE or 4WIRE as necessary
+}
+
+
+void loop() {
+  GetTemp(&thermo1, 1);
+  GetTemp(&thermo2, 2);
+  GetTemp(&thermo3, 3);
+  delay(1000);
+}
+
+void GetTemp(Adafruit_MAX31865 *thermo, int num)
+{
+  uint16_t rtd = thermo->readRTD();
+
+  Serial.print("RTD value: "); Serial.println(rtd);
+  float ratio = rtd;
+  ratio /= 32768;
+  Serial.print("Ratio = "); Serial.println(ratio,8);
+  Serial.print("Resistance = "); Serial.println(RREF*ratio,8);
+  Serial.print("Temperature = "); Serial.println(thermo->temperature(RNOMINAL, RREF));
+  Serial.print("RTD: "); Serial.println(num); 
+
+  // Check and print any faults
+  uint8_t fault = thermo->readFault();
+  if (fault) {
+    Serial.print("Fault 0x"); Serial.println(fault, HEX);
+    if (fault & MAX31865_FAULT_HIGHTHRESH) {
+      Serial.println("RTD High Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_LOWTHRESH) {
+      Serial.println("RTD Low Threshold"); 
+    }
+    if (fault & MAX31865_FAULT_REFINLOW) {
+      Serial.println("REFIN- > 0.85 x Bias"); 
+    }
+    if (fault & MAX31865_FAULT_REFINHIGH) {
+      Serial.println("REFIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_RTDINLOW) {
+      Serial.println("RTDIN- < 0.85 x Bias - FORCE- open"); 
+    }
+    if (fault & MAX31865_FAULT_OVUV) {
+      Serial.println("Under/Over voltage"); 
+    }
+    thermo->clearFault();
+  }
+  Serial.println();
+  //delay(1000);
+}
