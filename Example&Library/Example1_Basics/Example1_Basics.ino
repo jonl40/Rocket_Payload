2dd40c71e095f18e9ab43253ff8974203b412488
jonl40 <jonathanluu122@gmail.com>
2021-11-22 21:55:20 -0500

    increased imu sampling rate

--- Example&Library/Example1_Basics/Example1_Basics.ino
+++ Example&Library/Example1_Basics/Example1_Basics.ino
@@ -69,7 +69,7 @@ void loop() {
     myICM.getAGMT();                // The values are only updated when you call 'getAGMT'
 //    printRawAGMT( myICM.agmt );     // Uncomment this to see the raw values, taken directly from the agmt structure
     printScaledAGMT( myICM.agmt);   // This function takes into account the sclae settings from when the measurement was made to calculate the values with units
-    delay(30);
+    // delay(30);
   }else{
     Serial.println("Waiting for data");
     delay(500);
